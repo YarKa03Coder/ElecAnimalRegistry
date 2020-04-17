5aecd8cea0bd7f81390ce572925f3dbd2e78cdf6
Leriatod <artem.viznuk@gmail.com>
2020-04-18 00:45:15 +0300

    Added my library from PC that accesses the database

---
+++ DataAccess/Models/PetModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class PetModel
+    {
+        public int UniqueID { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string AnimalType { get; set; }
+        public string BreedType { get; set; }
+        public string Gender { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
