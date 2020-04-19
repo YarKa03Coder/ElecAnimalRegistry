8df63ebeb0a599596c1d42a3040d6da513d90beb
Leriatod <artem.viznuk@gmail.com>
2020-04-19 17:33:49 +0300

    Added my project and moved library inside the project

---
+++ DatabaseTestingConsoleApp/WebApiHelper/Models/PetModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiHelper.Models
+{
+    public class PetModel
+    {
+        public int UniqueID { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string AnimalType { get; set; }
+        public string BreedType { get; set; }
+        public string Gender { get; set; }
+        public string ImagePath { get; set; }
+
+        public string AgeInStringFormat 
+        { 
+            get
+            {
+                DateTime now = DateTime.Today;
+
+                int months = now.Month - BirthDate.Month;
+                int years = now.Year - BirthDate.Year;
+
+                if (now.Day < BirthDate.Day)
+                {
+                    months--;
+                }
+
+                if (months < 0)
+                {
+                    years--;
+                    months += 12;
+                }
+
+                int days = (now - BirthDate.AddMonths(years * 12 + months)).Days;
+
+                return String.Format("{0} year{1}, {2} month{3}, {4} day{5}",
+                                      years,  (years == 1) ? "" : "s",
+                                      months, (months == 1) ? "" : "s",
+                                      days,   (days == 1) ? "" : "s");
+            }
+        }
+    }
+}
---
+++ DatabaseTestingConsoleApp/WebApiHelper/Models/RegisterUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiHelper.Models
+{
+    public class RegisterUserModel : FrontendUserModel
+    {
+        public string Password { get; set; }
+    }
+}
