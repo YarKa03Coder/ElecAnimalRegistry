1ba6e794f1865cc5636e698607d2773baf2a9845
YarKa03Coder <yaroslav0302001@gmail.com>
2020-05-05 15:07:20 +0300

    Modify methods in RegexCheck.cs

--- ApplicationOOP/ApplicationOOP/ApplicationOOP/RegexCheck.cs
+++ ApplicationOOP/ApplicationOOP/ApplicationOOP/RegexCheck.cs
@@ -9,49 +9,36 @@ namespace ApplicationOOP
     {
         public static bool ValidEmail(string email)
         {
-            if (new System.Net.Mail.MailAddress(email).Address.Equals(email))
-                return true;
-            else
-                return false;
+            return new System.Net.Mail.MailAddress(email).Address.Equals(email);
         }
 
         public static bool Valid_firstName(string first_name)
         {
-            if (String.IsNullOrEmpty(first_name))
-                return false;
-            else
-                return true;
-
+            return String.IsNullOrEmpty(first_name);
         }
 
         public static bool Valid_lastName(string last_name)
         {
-            if (String.IsNullOrEmpty(last_name))
-                return false;
-            else
-                return true;
-
+            return String.IsNullOrEmpty(last_name);
         }
 
         public static bool ValidPassword(string password)
         {
             var password_pattern = "^((?!.*[\\s])(?=.*[A-Z])(?=.*\\d).{7,12})";
 
-            if (Regex.IsMatch(password, password_pattern))
-                return true;
-            else
-                return false;
-            //return true;
+            return Regex.IsMatch(password, password_pattern);
         }
 
         public static bool ValidPhone(string phone)
         {
             var phone_pattern = "^\\+?3?8?(0[\\s\\.-]\\d{2}[\\s\\.-]\\d{3}[\\s\\.-]\\d{2}[\\s\\.-]\\d{2})$";
 
-            if (Regex.IsMatch(phone, phone_pattern))
-                return true;
-            else
-                return false;
+            return Regex.IsMatch(phone, phone_pattern);
+        }
+
+        public static bool ConfirmPassword(string passwordconfirm, Entry entred_password)
+        {
+            return passwordconfirm.Equals(entred_password.Text);
         }
     }
 }
